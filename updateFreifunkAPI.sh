80212bbba1a55005c1203e33e47c2a177ce3395b
SRV01 <amperpirat@gmx.de>
2014-11-28 18:35:46 +0100

    updated number of nodes

--- updateFreifunkAPI.sh
+++ updateFreifunkAPI.sh
@@ -7,4 +7,4 @@ python freifunkAPIupdater.py
 
 git add freifunk.net.json
 git commit -a -m "updated number of nodes"
-git push git@github.com/freifunkMUC/freifunk.net-API.git
+git push git@github.com:freifunkMUC/freifunk.net-API.git
