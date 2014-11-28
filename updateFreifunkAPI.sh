0d17260aadc8a05582aefb88b3abd7fd759dc383
SRV01 <amperpirat@gmx.de>
2014-11-28 18:34:30 +0100

    using ssh auth for github

--- updateFreifunkAPI.sh
+++ updateFreifunkAPI.sh
@@ -2,11 +2,9 @@
 
 wget http://37.120.168.150/node_stats -O node_stats
 
-git.sh -i /var/www/.ssh/srv01 pull
+git pull
 python freifunkAPIupdater.py
 
 git add freifunk.net.json
 git commit -a -m "updated number of nodes"
-git.sh -i /var/www/.ssh/srv01 push https://chris007de@github.com/freifunkMUC/freifunk.net-API.git
-
-
+git push git@github.com/freifunkMUC/freifunk.net-API.git
