59c65a05ba198b280af3b0e3246e51fd81724fe4
x29a <0.x29a.0@gmail.com>
2016-02-08 11:52:37 +0100

    revert previous changes, update url to nodes.json (instead of nodelist.json) and incorporate fixes from PR 3 by rotanid

--- node_stats.py
+++ node_stats.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 from requests import get as rget
 
-NODESJSON = 'https://ffmuc.net/data/nodelist.json'
+NODESJSON = 'https://ffmuc.net/data/nodes.json'
 
 def scrape(url):
     '''returns remote json'''
@@ -18,10 +18,10 @@ if __name__ == '__main__':
         online = 0
         nonclient = 0
 
-        for node in nodes['nodes']:
-            if node['status']['online']:
+        for node in nodes['nodes'].values():
+            if node['flags']['online']:
                 online += 1
-                if not node['status']['clients']:
+                if node['flags']['gateway']:
                     nonclient += 1
 
         now = datetime.now().strftime('%H:%M %d.%m.%Y')
